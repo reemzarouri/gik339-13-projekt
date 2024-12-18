f8559222b45395dcc54d9b472ccf19b8c7b32c35
reemzarouri <59601968+reemzarouri@users.noreply.github.com>
2024-12-18 17:02:47 +0100

    1.22

--- script.js
+++ script.js
@@ -106,8 +106,8 @@ async function saveCar(event) {
     const message = id ? "Car updated successfully" : "Car added successfully";
     //showToast(message, "success");
     //alert(message);
-    console.log(message);
-    showMessage(message, "success", [
+    console.log(id ? "Car updated successfully" : "Car added successfully");
+    showMessage(message, id ? "Car updated successfully" : "Car added successfully", [
       {
         label: "Ok",
         class: "btn-primary",
